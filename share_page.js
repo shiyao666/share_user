cdbcf1e1d3404d7053c799db8eee2bfcf86bfb9d
shiyao666 <1102699514@qq.com>
2018-07-23 13:34:24 +0800

    new_share

--- share_page.js
+++ share_page.js
@@ -5,7 +5,7 @@ $("body").on("click", ".success_share", function share_a() {
         type: "post",
         data: {
             "****": "***",
-        
+
         },
         async: true,
         success: function (result) {
@@ -21,4 +21,25 @@ $(document).ready(function () {
     $(".yaoqing_span1").html($('#name').val() + ",邀请您参加");
     $(".banner2").css("margin-top", " 20px")
 
+})
+var share_id =null;
+$(document).ready(function () {
+$.get('http://192.168.4.53/index.php?m=invform&c=phone&a=index',function(msg){
+    share_id= msg.id
+})    
+})
+$(document).ready(function () {
+    $.ajax({
+        type: 'post',
+        url: 'http://192.168.4.53/index.php?m=invform&c=phone&a=index',
+        dataType: "json",
+        data:{
+            "share_id":share_id
+        },
+        success: function () {
+            
+        }, error: function () {
+            return false;
+        }
+    })      
 })
